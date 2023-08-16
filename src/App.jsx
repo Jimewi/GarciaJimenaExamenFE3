6b0386b00a65fb8551258a67db9f5ef233efd266
Jimewi <garciajimena89@gmail.com>
2023-08-16 01:12:33 -0300

    comenzando

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  
+  return (
+    <>
+      <div className='App'>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+          <Card />
+        <h1>Éxitos!</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
